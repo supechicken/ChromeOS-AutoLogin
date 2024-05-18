755baf9ff2519414dccb0a296c274b6e760ce864
supechicken <supechicken666@gmail.com>
2024-05-18 18:11:34 +0800

    Handle rootfs remount

--- install.sh
+++ install.sh
@@ -1,6 +1,51 @@
 #!/bin/bash
 set -eu
 
+# prevent conflict between system libraries and Chromebrew libraries
+unset LD_LIBRARY_PATH
+export PATH=/bin:/sbin:/usr/bin:/usr/sbin
+
+function remount_rootfs() {
+  echo '[+] Trying to remount root filesystem in read/write mode...'
+  mount -o remount,rw / && return 0 || return $?
+}
+
+function remove_rootfs_verification() {
+  # KERN-A B for arm  ROOT-A B for x64
+  if [[ "$ARCH" =~ "arm64" ]];then
+    /usr/share/vboot/bin/make_dev_ssd.sh --remove_rootfs_verification --partitions 2
+    /usr/share/vboot/bin/make_dev_ssd.sh --remove_rootfs_verification --partitions 4
+  else
+    /usr/share/vboot/bin/make_dev_ssd.sh --remove_rootfs_verification --partitions 3
+    /usr/share/vboot/bin/make_dev_ssd.sh --remove_rootfs_verification --partitions 5
+  fi
+  echo -e "${YELLOW}Please run this script again after reboot.${RESET}"
+  echo '[+] Rebooting in 3 seconds...'
+  sleep 3
+  reboot
+}
+
+if [[ ${EUID} != 0 ]]; then
+  echo -e "${RED}Please run this script as root.${RESET}" >&2
+  exit 1
+fi
+
+if ! remount_rootfs; then
+  echo -e "${YELLOW}Remount failed. Did you disable rootFS verification?${RESET}" >&2
+  read -N1 -p 'Disable rootFS verification now? (This will reboot your system) [Y/n]: ' response
+  echo -e "\n"
+
+  case $response in
+  Y|y)
+    remove_rootfs_verification
+  ;;
+  *)
+    echo 'No changes made.'
+    exit 1
+  ;;
+  esac
+fi
+
 cd /tmp
 
 echo '[+] Downloading ChromeOS-AutoLogin...'
